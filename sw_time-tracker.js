39d42acda83792ce479fc312c342ef71e5ff027b
blsgamesy-hub <bls.game.sy@gmail.com>
2026-05-28 05:06:53 +0900

    Add files via upload

--- sw_time-tracker.js
+++ sw_time-tracker.js
@@ -1,16 +1,13 @@
-const CACHE_NAME = 'time-tracker-v2';
-const ASSETS = [
+const CACHE_NAME = 'time-tracker-v3';
+const CORE_ASSETS = [
   './time-tracker.html',
   './manifest_time-tracker.json',
-  './icon_192_time-tracker.png',
-  './icon_512_time-tracker.png',
-  'https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Mono:wght@300;400;500&family=Noto+Sans+KR:wght@300;400;500&display=swap'
 ];
 
-// 설치: 핵심 파일 캐시
+// 설치: 핵심 파일만 캐시 (실패해도 설치 계속)
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
   );
   self.skipWaiting();
 });
@@ -32,25 +29,12 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        // 성공하면 캐시 갱신 후 반환
         if (res && res.status === 200) {
           const clone = res.clone();
           caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
         }
         return res;
       })
-      .catch(() => {
-        // 오프라인이면 캐시에서
-        return caches.match(e.request);
-      })
-  );
-});
-        if (res && res.status === 200 && res.type !== 'opaque') {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => cached);
-    })
+      .catch(() => caches.match(e.request))
   );
 });
